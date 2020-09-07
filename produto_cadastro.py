a5db8a96e83b9d4e1ef8b9c8fb7ed49773d8b7a5
Jordana-Nunes <53192683+Jordana-Nunes@users.noreply.github.com>
2020-09-07 19:03:13 -0300

    Add files via upload

---
+++ produto_cadastro.py
@@ -0,0 +1,22 @@
+#cadastro_produto
+nome_do_produto = input("nome do produto")
+print(nome_do_produto)
+
+breve_descricao_do_produto = input("breve descrição do produto")
+print(breve_descricao_do_produto)
+
+foto_do_produto = input(" foto do produto")
+print(foto_do_produto)
+
+tipo_de_produto = input("tipo de produto")
+print(tipo_de_produto)
+
+cnpj = input("CNPJ")
+print(cnpj)
+
+cpf = input("CPF")
+print(cpf)
+
+
+def py():
+    return None

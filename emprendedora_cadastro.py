48d26318be3175c29f8a7f2d22290642c567a15a
Jordana-Nunes <53192683+Jordana-Nunes@users.noreply.github.com>
2020-09-07 19:02:06 -0300

    Add files via upload

---
+++ emprendedora_cadastro.py
@@ -0,0 +1,20 @@
+#cadastro_empreededora
+def cadastar(empreededora):
+    cadastro = dict()
+from datetime import datetime
+
+cadastro = dict()
+nome_completo = input("Nome completo:")
+nasc = int(input("Ano de nascimento:"))
+cadastro['idade'] = datetime.now().year - nasc
+endereco = input("endereço")
+telefone = int(input("telefone"))
+email = input("e-mail")
+rg = input("RG")
+cpf = input("CPF")
+escolaridade = input("escolaridade")
+empreededora = cadastro
+
+
+def py():
+    return None

e3c120bedb9c5249887fde2ace8cae44fc4bbe6d
Jordana-Nunes <53192683+Jordana-Nunes@users.noreply.github.com>
2020-09-07 19:02:38 -0300

    Add files via upload

---
+++ empresa_cadastro.py
@@ -0,0 +1,12 @@
+#cadastro_da_empresa
+cadastro1 = dict()
+cadastro1 ['nome_da_empresa'] = str( input("nome da empresa"))
+
+telefone_da_empresa = input("telefone da emppresa")
+email = input("e-mail da empresa")
+CNPJ= input("CNPJ")
+responsalvel_pela_empresa = input("responsalvel pela empresa")
+endereco_da_empresa = input("endereco da empresa")
+data_de_fundacao = input("data de fundação")
+
+cadastro1['alvara da empresa'] = int(input("alvara_da_empresa(0 não tem):"))
